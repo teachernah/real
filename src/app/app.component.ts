fda2ce1c21c58e5ed94b2bb0dcb5c8db80bad59f
teachernah <naheed.hanif.124@gmail.com>
2018-04-08 11:48:15 +0500

    Property binding works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = '';
+  allowNewServer = false;
   serverId = 10;
   serverStatus = 'offline';
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  ngOnInit() {
+  }
 }
