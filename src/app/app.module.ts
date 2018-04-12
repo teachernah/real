d87da11b6395a0d92c761d2ca1e3a606da737a79
teachernah <naheed.hanif.124@gmail.com>
2018-04-12 20:35:09 +0500

    Font Awesome added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -22,7 +23,8 @@ const appRoutes: Routes = [];
     RouterModule.forRoot(
       appRoutes,
       { enableTracing: true }
-    )
+    ),
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
